adf9835ebbd51d4236f5441b756a0911311a3876
Reilly Keele <rkeele@ualberta.ca>
2020-02-28 15:50:08 -0700

    Update User.java

--- code/app/src/main/java/com/example/android/arrival/Model/User.java
+++ code/app/src/main/java/com/example/android/arrival/Model/User.java
@@ -4,9 +4,11 @@ public class User {
 
     private String username;
     private String password;
+    private String name;
 
-    public User() {
-
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 
 
