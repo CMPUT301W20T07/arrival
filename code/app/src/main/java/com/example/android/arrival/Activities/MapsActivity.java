9a6b0fe7d954973f54a2356a891c9c8e5cf5f848
Jessica Brownell <jessbrownell09@gmail.com>
2020-03-02 10:31:55 -0700

    Checking new API key

--- code/app/src/main/java/com/example/android/arrival/Activities/MapsActivity.java
+++ code/app/src/main/java/com/example/android/arrival/Activities/MapsActivity.java
@@ -30,7 +30,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
