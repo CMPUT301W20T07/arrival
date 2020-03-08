99f29f1cee7eeb589d4200ee29260c4d63fc4e4b
reillykeele <54957139+reillykeele@users.noreply.github.com>
2020-03-08 13:12:25 -0600

    Implement RequestCallbackListener

--- code/app/src/main/java/com/example/android/arrival/Activities/MainActivity.java
+++ code/app/src/main/java/com/example/android/arrival/Activities/MainActivity.java
@@ -4,18 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.android.arrival.Model.GeoLocation;
 import com.example.android.arrival.Model.Request;
+import com.example.android.arrival.Model.RequestCallbackListener;
 import com.example.android.arrival.Model.RequestManager;
 import com.example.android.arrival.Model.Rider;
 import com.example.android.arrival.R;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RequestCallbackListener {
 
     private static final String TAG = "main-activity";
 
@@ -48,14 +51,42 @@ public class MainActivity extends AppCompatActivity {
         rm.openRequest(req2);
         req.setFare(4.20f);
         rm.updateRequest(req);
-        rm.getRiderRequests("user");
+        rm.getRiderRequests("user", this);
         rm.deleteRequest(req.getID());
         rm.deleteRequest(req1.getID());
         rm.deleteRequest(req2.getID());
+
+        rm.getOpenRequests(this);
     }
 
     public void openMaps(){
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onCallbackStart() {
+
+    }
+
+    @Override
+    public void onGetOpenSuccess(QuerySnapshot snapshot){
+        // Convert the snapshot to objects that can be used to display information
+        List<Request> openRequests = snapshot.toObjects(Request.class);
+        Log.d("MainActivity" + "-getOpen", openRequests.toString());
+    }
+
+    @Override
+    public void onGetRiderRequestsSuccess(QuerySnapshot snapshot) {
+        // Convert the snapshot to objects that can be used to display information
+        List<Request> userRequests = snapshot.toObjects(Request.class);
+        Log.d("MainActivity" + "-getReq", userRequests.toString());
+    }
+
+    @Override
+    public void onGetDriverRequestsSuccess(QuerySnapshot snapshot) {
+        // Convert the snapshot to objects that can be used to display information
+        List<Request> userRequests = snapshot.toObjects(Request.class);
+        Log.d("MainActivity" + "-getReq", userRequests.toString());
     }
 }
--- code/app/src/main/java/com/example/android/arrival/Model/RequestManager.java
+++ code/app/src/main/java/com/example/android/arrival/Model/RequestManager.java
@@ -3,12 +3,16 @@ package com.example.android.arrival.Model;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 import java.util.List;
 
@@ -20,7 +24,9 @@ public class RequestManager {
     public static final String REQUESTS = "requests";
     public static final String TAG = "RequestManager";
 
-    public static RequestManager instance;
+    private static RequestManager instance;
+    private FirebaseFirestore db;
+    private CollectionReference requestRef;
 
     /**
      * Returns the RequestManager object associated with the current Android application.
@@ -36,7 +42,19 @@ public class RequestManager {
     }
 
     private RequestManager() {
+        db = FirebaseFirestore.getInstance();
+        requestRef = db.collection(REQUESTS);
+        requestRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                // If error
+                if(e != null) {
+                    return;
+                }
 
+                // Notify all
+            }
+        });
     }
 
     /**
@@ -44,13 +62,11 @@ public class RequestManager {
      * @param req a Request object to be opened in FireStore.
      */
     public void openRequest(Request req) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-        db.collection("requests").document(req.getID()).set(req).addOnSuccessListener(new OnSuccessListener<Void>() {
+        requestRef.document(req.getID()).set(req).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
                 Log.d(TAG + "-open", "Successfully opened new request " + req.getID() + " in FireStore.");
-                getOpenRequests(); // Update current list of open requests
+//                getOpenRequests(); // Update current list of open requests
             }
         });
     }
@@ -60,8 +76,7 @@ public class RequestManager {
      * @param id
      */
     public void deleteRequest(String id) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("requests").document(id).delete()
+        requestRef.document(id).delete()
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
@@ -82,10 +97,7 @@ public class RequestManager {
      * @param req
      */
     public void updateRequest(Request req) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        DocumentReference requestRef = db.collection(REQUESTS).document(req.getID());
-
-        db.collection(REQUESTS).document(req.getID())
+        requestRef.document(req.getID())
                 .set(req)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
@@ -104,32 +116,31 @@ public class RequestManager {
     /**
      * Retrieves all currently open requests from the FireStore Cloud Database.
      */
-    public void getOpenRequests() {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        CollectionReference requestsRef = db.collection(REQUESTS);
-        requestsRef.whereEqualTo("status", Request.STATUS_OPEN).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+    public void getOpenRequests(final RequestCallbackListener listener) {
+        requestRef.whereEqualTo("status", Request.STATUS_OPEN).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                 Log.d(TAG + "-getOpen", "Successfully retrieved open requests from DB. ");
-                List<Request> openRequests = queryDocumentSnapshots.toObjects(Request.class);
-                Log.d(TAG + "-getOpen", openRequests.toString());
+
+
+                listener.onGetOpenSuccess(queryDocumentSnapshots);
             }
         });
     }
 
     /**
-     * Retrieves all the requests made by the rider from the FireStore Cloud Database.
+     * Retrieves all the requests made by the given rider from the FireStore Cloud Database.
      */
-    public void getRiderRequests(String rider) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        CollectionReference requestsRef = db.collection(REQUESTS);
-        requestsRef.whereEqualTo("rider", rider).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+    public void getRiderRequests(String rider, final RequestCallbackListener listener) {
+        requestRef.whereEqualTo("rider", rider).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                 Log.d(TAG + "-getRider", "Successfully retrieved rider " + rider + "+'s requests from DB. ");
                 List<Request> userRequests = queryDocumentSnapshots.toObjects(Request.class);
                 if(userRequests.size() > 0) {
                     Log.d(TAG + "-getRider", userRequests.toString());
+
+                    listener.onGetRiderRequestsSuccess(queryDocumentSnapshots);
                 } else {
                     Log.d(TAG + "-getRider", "No Requests matched the username provided. ");
                 }
@@ -138,18 +149,18 @@ public class RequestManager {
     }
 
     /**
-     * Retrieves all the requests accepted by the driver from the FireStore Cloud Database.
+     * Retrieves all the requests accepted by the given driver from the FireStore Cloud Database.
      */
-    public void getDriverRequests(String driver) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        CollectionReference requestsRef = db.collection(REQUESTS);
-        requestsRef.whereEqualTo("driver", driver).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+    public void getDriverRequests(String driver, final RequestCallbackListener listener) {
+        requestRef.whereEqualTo("driver", driver).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                 Log.d(TAG + "-getDriv", "Successfully retrieved driver " + driver + "'s requests from DB. ");
                 List<Request> userRequests = queryDocumentSnapshots.toObjects(Request.class);
                 if(userRequests.size() > 0) {
                     Log.d(TAG + "-getDriv", userRequests.toString());
+
+                    listener.onGetDriverRequestsSuccess(queryDocumentSnapshots);
                 } else {
                     Log.d(TAG + "-getDriv", "No Requests matched the username provided. ");
                 }
