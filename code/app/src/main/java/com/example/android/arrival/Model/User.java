33e4191a2b8fc5d86a1f46fb962ec17c7f2c9930
Sirak Radaa <60719924+sirakradaa@users.noreply.github.com>
2020-03-13 14:55:16 -0600

    Revert "Drivermaps"

--- code/app/src/main/java/com/example/android/arrival/Model/User.java
+++ code/app/src/main/java/com/example/android/arrival/Model/User.java
@@ -1,41 +1,4 @@
 package com.example.android.arrival.Model;
 
 public class User {
-
-    private String username;
-    private String password;
-    private String name;
-
-    public User() {
-        // Must have a constructor with no params to be pulled as Object from FireStore.
-    }
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
