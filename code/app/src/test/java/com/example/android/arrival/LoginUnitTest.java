63299449c1650d56543657ad0dd62f6b003db368
Keshinee <kesh.moonian@gmail.com>
2020-03-27 22:19:56 -0600

    modified unit tests

--- code/app/src/test/java/com/example/android/arrival/LoginUnitTest.java
+++ code/app/src/test/java/com/example/android/arrival/LoginUnitTest.java
@@ -1,5 +1,7 @@
 package com.example.android.arrival;
 
+import android.widget.EditText;
+
 import com.example.android.arrival.Activities.LoginActivity;
 
 import org.junit.Test;
@@ -8,27 +10,33 @@ import static junit.framework.TestCase.assertTrue;
 
 public class LoginUnitTest {
 
-    LoginActivity email;
-    LoginActivity password;
+    EditText email;
+    EditText password;
 
 
     @Test
     void testEmptyEmailField() {
 
         LoginActivity emptyEmail = new LoginActivity();
-        assertTrue(email.toString().equals(""));
+        email = emptyEmail.findViewById(R.id.login_email_editText);
+        String email_text = String.valueOf(email.getText());
+        assertTrue(email_text.equals(""));
     }
 
     @Test
     void testValidEmail() {
         LoginActivity validEmail = new LoginActivity();
-        assertTrue(email.toString().contains("@"));
+        email = validEmail.findViewById(R.id.login_email_editText);
+        String email_text = String.valueOf(email.getText());
+        assertTrue(email_text.contains("@"));
     }
 
     @Test
     void testPasswordEmailField() {
 
         LoginActivity EmptyPassword = new LoginActivity();
-        assertTrue(password.toString().equals(""));
+        password = EmptyPassword.findViewById(R.id.login_passWord_editText);
+        String password_text = String.valueOf(password.getText());
+        assertTrue(password_text.equals(""));
     }
 }
